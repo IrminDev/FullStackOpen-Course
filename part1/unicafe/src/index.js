1a52f728bb12d2216548c78352cde9c814dc51b2
IrminDev <ihernandezj1902@alumno.ipn.mx>
2023-07-06 18:13:38 -0600

    Commit for exercise 1.11

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client'
 
 const StatisticLine = (props) => {
   return (
-    <p>{props.text} {props.value}</p>
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
   )
 }
 
@@ -14,14 +17,16 @@ const Statistics = (props) => {
       {props.stats.total === 0 ? (
         <p>No feedback given</p>
       ) : (
-        <div>
-          <StatisticLine text="good" value={props.stats.good} />
-          <StatisticLine text="neutral" value={props.stats.neutral} />
-          <StatisticLine text="bad" value={props.stats.bad} />
-          <StatisticLine text="all" value={props.stats.total} />
-          <StatisticLine text="average" value={props.stats.average} />
-          <StatisticLine text="positive" value={props.stats.positive + " %"} />
-        </div>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={props.stats.good} />
+            <StatisticLine text="neutral" value={props.stats.neutral} />
+            <StatisticLine text="bad" value={props.stats.bad} />
+            <StatisticLine text="all" value={props.stats.total} />
+            <StatisticLine text="average" value={props.stats.average} />
+            <StatisticLine text="positive" value={props.stats.positive + " %"} />
+          </tbody>
+        </table>
       )}
     </>
   )
