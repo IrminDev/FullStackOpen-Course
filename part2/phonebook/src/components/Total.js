3bf1b4432a61f92100e0bfc0d5bb2771d6fc96c6
IrminDev <ihernandezj1902@alumno.ipn.mx>
2023-07-07 18:15:29 -0600

    Commit for exercise 2.6

---
+++ part2/phonebook/src/components/Total.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Total = ({total}) => {
+  return (
+    <div>Total of {total} exercises</div>
+  )
+}
+
+export default Total
---
+++ part2/phonebook/src/index.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', id: 1 }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addPerson = (e) => {
+    e.preventDefault();
+    const personObject = {
+      name: newName,
+      id: persons.length + 1
+    }
+    setPersons(persons.concat(personObject))
+  }
+
+  const handlePersonChange = (e) => {
+    setNewName(e.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input onChange={handlePersonChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <div key={person.name}>{person.name}</div>)}
+    </div>
+  )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
