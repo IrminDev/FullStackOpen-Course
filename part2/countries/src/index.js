6cac26fc16f99af38204bb4672c48d29926cab15
IrminDev <ihernandezj1902@alumno.ipn.mx>
2023-07-07 20:33:41 -0600

    Commit for exercise 2.14

--- part2/countries/src/index.js
+++ part2/countries/src/index.js
@@ -2,7 +2,20 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import ReactDOM from 'react-dom/client'
 
+const api_key = process.env.REACT_APP_API_KEY
+
 const Country = ({country}) => {
+  const [ weather, setWeather ] = useState({})
+
+  // The API doesn't let me access, always throw the error 105
+  useEffect(() => {
+    axios.get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}`)
+    .then((response) => {
+      setWeather(response.data)
+      console.log(response.data)
+    })
+  }, [])
+
   return (
     <div>
       <h1>{country.name.common}</h1>
@@ -13,6 +26,10 @@ const Country = ({country}) => {
         {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
       </ul>
       <img src={country.flags.png} alt={country.flags.alt} width="200" height="200" />
+      <h2>Weather in {country.capital}</h2>
+      <p>temperature: {weather.current.temperature} Celcius</p>
+      <img src={weather.current.weather_icons[0]} alt={weather.current.weather_descriptions[0]} />
+      <p>wind: {weather.current.wind_speed} mph direction {weather.current.wind_dir}</p>
     </div>
   )
 }
@@ -41,7 +58,14 @@ const App = () => {
           <p>Too many matches, specify another filter</p> :
           countriesToShow.length === 1 ?
           <Country country={countriesToShow[0]} /> :
-          countriesToShow.map(country => <p key={country.name.common}>{country.name.common}</p>)
+          countriesToShow.map(country => {
+            return (
+              <div key={country.name.common}>
+                {country.name.common}
+                <button onClick={() => setFilter(country.name.common)}>show</button>
+              </div>
+            )
+          })
         }
     </div>
   )
