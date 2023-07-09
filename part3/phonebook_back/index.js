92a4f5b51bfe1e4a24442e04aca04ae9acdaae8e
IrminDev <ihernandezj1902@alumno.ipn.mx>
2023-07-09 17:24:11 -0600

    Commit for exercise 3.6

--- part3/phonebook_back/index.js
+++ part3/phonebook_back/index.js
@@ -55,6 +55,25 @@ app.delete('/api/persons/:id', (request, response) => {
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({ 
+        error: 'name missing' 
+        })
+    }
+
+    if (!body.number) {
+        return response.status(400).json({ 
+        error: 'number missing' 
+        })
+    }
+
+    if (persons.find(person => person.name === body.name)) {
+        return response.status(400).json({ 
+        error: 'name must be unique' 
+        })
+    }
+
     const person = {
         name: body.name,
         number: body.number,
