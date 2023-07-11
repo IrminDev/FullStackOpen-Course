bfd808188ee40011f62e4e08cf115a67ca161a94
IrminDev <ihernandezj1902@alumno.ipn.mx>
2023-07-11 16:21:24 -0600

    Commit for exercise 4.4

--- part4/blog-app/tests/list.test.js
+++ part4/blog-app/tests/list.test.js
@@ -5,3 +5,52 @@ test('dummy returns one', () => {
     const result = list_helper.dummy(blogs)
     expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+    const listWithOneBlog = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          likes: 5,
+          __v: 0
+        }
+    ]
+
+    const emptyList = []
+
+    const biggerList = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    test('when list has only one blog, equals the likes of that', () => {
+    const result = list_helper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+    })
+
+    test('of empty list is zero', () => {
+        const result = list_helper.totalLikes(emptyList)
+        expect(result).toBe(0)
+    })
+
+    test('of a bigger list is calculated right', () => {
+        const result = list_helper.totalLikes(biggerList)
+        expect(result).toBe(10)
+    })
+})
