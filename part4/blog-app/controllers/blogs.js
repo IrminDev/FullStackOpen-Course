542d2c6f349777e3f8f50eb2876a9019adabdc20
IrminDev <ihernandezj1902@alumno.ipn.mx>
2023-07-11 20:13:24 -0600

    Commit for exercise 4.13

--- part4/blog-app/controllers/blogs.js
+++ part4/blog-app/controllers/blogs.js
@@ -23,4 +23,9 @@ blogRouter.post('/', async (request, response) => {
     response.status(201).json(blog)
 })
 
+blogRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+})
+
 module.exports = blogRouter
--- part4/blog-app/tests/blog_api.test.js
+++ part4/blog-app/tests/blog_api.test.js
@@ -72,6 +72,20 @@ test('blog without title or url is not added', async () => {
     .expect(400)
 })
 
+test('a blog can be deleted', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
+
+    const contents = blogsAtEnd.map(n => n.id)
+    expect(contents).not.toContain(blogToDelete.id)
+})
+
 afterAll(() => {
     mongoose.connection.close()
 })
