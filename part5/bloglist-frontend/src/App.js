51e863c882048122fb8977421602bdf52057d58e
IrminDev <ihernandezj1902@alumno.ipn.mx>
2023-07-13 16:58:37 -0600

    Commit for exercise 5.3 and 5.4

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -2,12 +2,17 @@ import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import Message from './components/Message'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [url, setUrl] = useState('');
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -33,6 +38,7 @@ const App = () => {
       window.localStorage.setItem('loggedBlogappUser', JSON.stringify(userLog))
       setUsername('')
       setPassword('')
+      blogService.setToken(userLog.token)
     } catch (exception) {
       console.log('Wrong credentials')
     }
@@ -44,9 +50,34 @@ const App = () => {
       setUser(null)
       window.localStorage.removeItem('loggedBlogappUser')
     } catch (exception) {
-      console.log('Wrong credentials')
+      setMessage('Wrong credentials')
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
     }
-  } 
+  }
+  
+  const handleCreate = async (event) => {
+    event.preventDefault()
+    try{
+      const blog = await blogService.create({
+        title, author, url,
+      })
+      setBlogs(blogs.concat(blog))
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+      setMessage(`a new blog ${blog.title} by ${blog.author} added`)
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
+    } catch (exception) {
+      setMessage('Wrong credentials')
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
+    }
+  }
 
   const loginForm = () => (
     <form onSubmit={handleLogin}>
@@ -76,6 +107,38 @@ const App = () => {
   const blogsForm = () => (
     <div>
       <h2>blogs</h2>
+      <form onSubmit={ handleCreate }>
+        <div>
+          title:
+          <input
+            type="text"
+            value={title}
+            name="title"
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          author:
+          <input
+            type="text"
+            value={author}
+            name="title"
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          URL:
+          <input
+            type="text"
+            value={url}
+            name="title"
+            onChange={({ target }) => setUrl(target.value)}
+          />
+        </div>
+
+        <button type="submit">new blog</button>
+      </form>
+
       <div>
         <p>{user.name} logged in</p> <button onClick={handleLogout}>logout</button>
       </div>
@@ -88,6 +151,7 @@ const App = () => {
   return (
     
     <div>
+      <Message message={message} />
       { user === null && loginForm() }
       { user !== null && blogsForm() }
     </div>
