de56b875adbe5a69c707592256824b8899b65711
IrminDev <ihernandezj1902@alumno.ipn.mx>
2023-07-12 18:27:27 -0600

    Commit for exercise 4.20

--- part4/blog-app/app.js
+++ part4/blog-app/app.js
@@ -8,6 +8,7 @@ const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
 
 mongoose.set('strictQuery', false)
 
@@ -20,6 +21,7 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
