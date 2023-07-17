df0303daf1707b938652fa370cae9e2d39e21a81
IrminDev <ihernandezj1902@alumno.ipn.mx>
2023-07-16 18:21:38 -0600

    Commit for exercise 6.24

--- part6/query-anecdotes/src/components/AnecdoteForm.js
+++ part6/query-anecdotes/src/components/AnecdoteForm.js
@@ -1,12 +1,20 @@
 import { useMutation, useQueryClient } from "react-query"
 import { createAnecdote } from "../requests"
+import { useNotificationDispatch } from "../NotificationContext"
 
 const AnecdoteForm = () => {
   const queryClient = useQueryClient()
+  const dispatch = useNotificationDispatch()
   const newAnecdoteMutation = useMutation(createAnecdote, {
     onSuccess: (newAnecdote) => {
       const anecdotes = queryClient.getQueryData('anecdotes')
       queryClient.setQueryData('anecdotes', [...anecdotes, newAnecdote])
+    },
+    onError: () => {
+      dispatch({type: 'SET_NOTIFICATION', data: 'An error occurred while creating anecdote, it must have length 5 or more'})
+      setTimeout(() => {
+        dispatch({type: 'CLEAR_NOTIFICATION'})
+      }, 5000)
     }
   })
 
