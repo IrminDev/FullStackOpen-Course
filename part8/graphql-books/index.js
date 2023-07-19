d9689f873d320b7eeff6d3fa07e270931f1920b1
IrminDev <ihernandezj1902@alumno.ipn.mx>
2023-07-19 16:49:49 -0600

    Commit for exercise 8.7

--- part8/graphql-books/index.js
+++ part8/graphql-books/index.js
@@ -108,6 +108,10 @@ const typeDefs = `
       published: Int!
       genres: [String!]!
     ) : Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ) : Author
   }
 `
 
@@ -130,6 +134,13 @@ const resolvers = {
         authors = authors.concat(author)
       }
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(author => author.name === args.name)
+      if (!author) return null
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(author => author.name === args.name ? updatedAuthor : author)
+      return updatedAuthor
     }
   }
 }
