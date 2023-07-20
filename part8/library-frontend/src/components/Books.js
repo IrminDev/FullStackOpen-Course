474dfd4c92c460e8815fccd0e4e20773a983b9b9
IrminDev <ihernandezj1902@alumno.ipn.mx>
2023-07-20 17:11:29 -0600

    Commit for exercise 8.19

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,8 +1,10 @@
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../queries'
+import { useState } from 'react'
 
 const Books = (props) => {
   const result = useQuery(ALL_BOOKS)
+  const [genre, setGenre] = useState('');
 
   if (!props.show) {
     return null
@@ -13,11 +15,15 @@ const Books = (props) => {
   }
 
   const books = result.data.allBooks
+  const genres = [...new Set(books.map(b => b.genres).flat())]
+  let bookSet = books
+  if(genre !== ''){
+    bookSet = books.filter(book => book.genres.includes(genre))
+  }
 
   return (
     <div>
       <h2>books</h2>
-
       <table>
         <tbody>
           <tr>
@@ -25,7 +31,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {bookSet.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -34,6 +40,7 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+      {genres.map(g => <button key={g} onClick={() => setGenre(g)}>{g}</button>)}
     </div>
   )
 }
