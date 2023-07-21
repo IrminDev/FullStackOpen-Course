37e2bc922aa6071b8ebe436c423db6be57cdd518
IrminDev <ihernandezj1902@alumno.ipn.mx>
2023-07-21 17:12:49 -0600

    Commit for exercise 9.5

--- part9/express-ts/index.ts
+++ part9/express-ts/index.ts
@@ -1,9 +1,23 @@
 import express = require ('express');
+import { calculateBMI, isNumber } from './utils/bmi';
 
 const app = express();
 
 app.get('/hello', (_req, res) => {
     res.send('Hello Full Stack!');
+})
+
+app.get('/bmi', (req, res) => {
+    const height = req.query.height;
+    const weight = req.query.weight;
+    if (!height || !weight) {
+        res.status(400).json({ error: "malformatted parameters" });
+    } else if (!isNumber(height) || !isNumber(weight)) {
+        res.status(400).json({ error: "malformatted parameters" });
+    } else {
+        const bmi = calculateBMI(Number(height)*0.01, Number(weight));
+        res.json({ height, weight, bmi });
+    }
 })
 
 const PORT = 3003;
