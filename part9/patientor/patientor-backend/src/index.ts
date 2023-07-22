af62abac6b42e5036c63413fcf79dc16246519c4
IrminDev <ihernandezj1902@alumno.ipn.mx>
2023-07-22 14:58:51 -0600

    Commit for exercise 9.10

--- part9/patientor/patientor-backend/src/index.ts
+++ part9/patientor/patientor-backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 const app = express();
+import diagnoseRouter from './routes/diagnoses';
 app.use(express.json());
 
 const PORT = 3003;
@@ -8,6 +9,8 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnoseRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
