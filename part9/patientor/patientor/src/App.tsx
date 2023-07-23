697cbf2208cc455d957fb3c37b3ff822a314cfde
IrminDev <ihernandezj1902@alumno.ipn.mx>
2023-07-22 20:20:44 -0600

    Commit for exercise 9.21

--- part9/patientor/patientor/src/App.tsx
+++ part9/patientor/patientor/src/App.tsx
@@ -1,13 +1,42 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Routes, useParams } from "react-router-dom";
 import { Button, Divider, Container, Typography } from '@mui/material';
-
+import FemaleIcon from '@mui/icons-material/Female';
+import MaleIcon from '@mui/icons-material/Male';
 import { apiBaseUrl } from "./constants";
 import { Patient } from "./types";
 
 import patientService from "./services/patients";
 import PatientListPage from "./components/PatientListPage";
+
+const PatientPage = ({patients} : {patients: Patient[]}) => {
+  const [patient, setPatient] = useState<Patient>()
+  const id = useParams<{ id: string }>().id;
+
+  useEffect(() => {
+    const fetchPatient = async () => {
+      if(id){
+        const patient = await patientService.getPatient(id);
+        setPatient(patient);
+      }
+    };
+    void fetchPatient();
+  }, [id]);
+
+  if (!patient) {
+    return null;
+  }
+
+  return (
+    <div>
+      <h1>{patient.name}</h1>
+      {patient.gender === 'male' ? <MaleIcon /> : <FemaleIcon />}
+      <p>ssn: {patient.ssn}</p>
+      <p>occupation: {patient.occupation}</p>
+    </div>
+  );
+}
 
 const App = () => {
   const [patients, setPatients] = useState<Patient[]>([]);
@@ -35,6 +64,7 @@ const App = () => {
           <Divider hidden />
           <Routes>
             <Route path="/" element={<PatientListPage patients={patients} setPatients={setPatients} />} />
+            <Route path="/patients/:id" element={<PatientPage patients={patients} />} />
           </Routes>
         </Container>
       </Router>
